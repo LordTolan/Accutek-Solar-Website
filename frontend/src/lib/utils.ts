f54bf391d3cb76ec9d06587e5a9e915bc54cfeeb
Clint Lenover <lenover.clint@accuteksolar.com>
2026-07-16 07:32:30 +0000

    Fix Customer Portal URL typo

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -16,7 +16,7 @@ export const API = \`\${BACKEND_URL}/api\`;
 export const HCP_BOOK_URL = "https://book.housecallpro.com/book/Accutek-Solar/a610e2efa0494a03ae59009369f2a058?v2=true";
 
 // Housecall Pro Customer Portal URL
-export const HCP_PORTAL_URL = "https://client.housecallpro.com#customer_portal/request-link?token=d2cca52d5dc74361b2c484f1306b70df";
+export const HCP_PORTAL_URL = "https://client.housecallpro.com/customer_portal/request-link?token=d2cca52d5dc74361b2c484f1306b70df";
 
 export function formatCurrency(n: number) {
   return n.toLocaleString("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 0 });
